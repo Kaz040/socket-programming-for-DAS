5fdad0cd6c8321445fe6a7a1a1b6f604c77e74a1
Kazeem Oladipupo <67549739+Kaz040@users.noreply.github.com>
2023-06-11 18:54:29 +0200

    new commit

---
+++ client/client.py
@@ -0,0 +1,43 @@
+import socket
+import threading
+client_network = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+friend_list = []
+
+
+#server_IP = str(input("Enter Server IP: "))
+
+#server_port = int(input("Enter Server Port "))
+
+def message_sender(client):
+
+    while True:
+        client_index = int(input())
+        message_to_send = input()
+        client_to_message = friend_list[client_index]
+        client.send(bytes(f"{client_to_message[0]}|{client_to_message[1]}|2|{message_to_send}", "utf-8"))
+
+def message_listener(client_network):
+    while True:
+        try:
+            data_received = client_network.recv(1024)
+            data = data_received.decode("utf-8")
+            if data != "":
+                data = data.split("|")
+                if data[2] == "1":
+                    friend_list.append((data[0],data[1]))
+                    print(friend_list)
+                elif data[2] == "2":
+                    print(f"[{data[0]}, {data[1]}]: {data[3]}")
+            
+
+        except KeyboardInterrupt:
+            client.close()
+
+
+client_network.connect(('192.168.0.103', 14500))
+messageListenerThread = threading.Thread(target=message_listener, args=(client_network, ))
+messageListenerThread.start()
+
+message_sender(client_network)
+
+    
---
+++ client_directory.py
@@ -0,0 +1,9 @@
+client_list =[]
+
+lista = [14, (34,89)]
+listb = [19, (87,94)]
+client_list.append(lista)
+client_list.append(listb)
+for client in client_list:
+    print(client[1][0])
+
